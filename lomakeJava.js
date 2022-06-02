21173d232694e3d5999c8b92c05e03d49d7a7118
r2-Mckellor-School <78972932+r2-Mckellor-School@users.noreply.github.com>
2022-06-02 13:27:18 +0300

    update

--- lomakeJava.js
+++ lomakeJava.js
@@ -8,3 +8,12 @@ for(var i = 0; i < radionapit.length; i++) {
 } if(radioTesti == false) {
  alert("Valitse tähti.");
 }}
+
+
+var email = formi.email.value;
+var ehdot = /\S+@\S+/;
+  if(!ehdot.test(email))
+  {
+    alert("Sähköpostisi ei ollut kunnollinen");
+    return false;
+  }
